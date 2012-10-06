72adf48391840b6c1e50760c5f03fc53233c05e9
Sean Kermes <skermes@gmail.com>
2012-10-06 14:15:11 -0700

    Adding colors to sites.

--- server/pentaton.js
+++ server/pentaton.js
@@ -2,15 +2,15 @@ Sites = new Meteor.Collection("sites");
 
 if (Sites.find().count() === 0) {
   var default_sites = [
-    {name: "KEXP", url: "http://kexp.org"},
-    {name: "last.fm", url: "http://last.fm/home"},
-    {name: "MOG", url: "http://mog.com"},
-    {name: "thesixtyone", url: "http://thesixtyone.com"},
-    {name: "Pandora", url: "http://pandora.com"},
-    {name: "mixest", url: "http://mixest.com"},
-    {name: "tunes.io", url: "http://tunes.io"},
-    {name: "Aurgasm", url: "http://aurgasm.us"},
-    {name: "Earbits", url: "http://www.earbits.com/play"}
+    {name: "KEXP", url: "http://kexp.org", color: "FEAC31"},
+    {name: "last.fm", url: "http://last.fm/home", color: "C70E14"},
+    {name: "MOG", url: "http://mog.com", color: "FF263A"},
+    {name: "thesixtyone", url: "http://thesixtyone.com", color: "4C4C4C"},
+    {name: "Pandora", url: "http://pandora.com", color: "0F6799"},
+    {name: "mixest", url: "http://mixest.com", color: "80CC70"},
+    {name: "tunes.io", url: "http://tunes.io", color: "0000FF"},
+    {name: "Aurgasm", url: "http://aurgasm.us", color: "067292"},
+    {name: "Earbits", url: "http://www.earbits.com/play", color: "292929"}
   ];
   for (var i = 0; i < default_sites.length; i++) {
     Sites.insert(default_sites[i]);

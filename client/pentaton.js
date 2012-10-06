06f4d4ad697391b12e8ed8bb306515a538de87fe
Sean Kermes <skermes@gmail.com>
2012-10-06 13:52:21 -0700

    Displaying sites in a grid.

--- client/pentaton.js
+++ client/pentaton.js
@@ -1,5 +1,19 @@
 Sites = new Meteor.Collection("sites");
 
-Template.site_list.sites = function() {
-  return Sites.find();
+Template.site_list.sites_by_3 = function() {
+  var groups = [];
+  var i = 0;
+  Sites.find().forEach(function(site) {
+    if (i % 3 === 0) {
+      groups.push([]);
+    }
+
+    groups[groups.length - 1].push(site);
+    i++;
+  });
+  return groups;
+}
+
+Template.site_row.sites = function() {
+  return this;
 }

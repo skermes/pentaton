1539822c53eff62436831262a859820966ba302a
Sean Kermes <skermes@gmail.com>
2015-09-23 13:25:20 -0400

    Render basic list of links.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sort"
 
 	"github.com/skermes/pentaton/Godeps/_workspace/src/github.com/zenazn/goji"
 	"github.com/skermes/pentaton/Godeps/_workspace/src/github.com/zenazn/goji/web"
@@ -16,11 +17,17 @@ import (
 
 var templates *template.Template
 var err error
+var db *sql.DB
 
 func setup() {
 	templates, err = template.ParseGlob("templates/*")
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error loading templates: %s", err.Error())
+	}
+
+	db, err = sql.Open("postgres", "postgres://skermes:skermes@localhost:5432/pentaton?sslmode=disable")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error opening database connection: %s", err.Error())
 	}
 }
 
@@ -32,38 +39,51 @@ func render(w io.Writer, tmpl string, data interface{}) {
 	}
 }
 
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	render(w, "hello", map[string]string{"Name": c.URLParams["name"]})
+type Link struct {
+	Url string
+	Name string
+	Color string
+	Position int
+	Category string
 }
 
-func row(c web.C, w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("postgres", "postgres://skermes:skermes@localhost:5432/pentaton?sslmode=disable")
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error connecting to database: %s", err.Error())
-		return
-	}
+type ByPosition []Link
 
-	rows, err := db.Query("select name, url, color from links limit 1")
+// Implement sort.Interface
+func (a ByPosition) Len() int            { return len(a) }
+func (a ByPosition) Swap(i, j int)       { a[i], a[j] = a[j], a[i] }
+func (a ByPosition) Less(i, j int) bool { return a[i].Position < a[j].Position }
+
+func links(c web.C, w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query(`
+		select url, links.name, color, position, categories.name from
+		links
+		join categories
+		on links.category = categories.id
+	`)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error reading from database: %s", err.Error())
 		return
 	}
 
-	rows.Next()
+	links := make([]Link, 0)
 
-	var name, url, color string
-	err = rows.Scan(&name, &url, &color)
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error scanning rows: %s", err.Error())
-		return
+	for rows.Next() {
+		var url, name, color, category string
+		var position int
+		if err := rows.Scan(&url, &name, &color, &position, &category); err != nil {
+			fmt.Fprintf(os.Stdout, "Error reading row: %s", err.Error())
+		}
+
+		links = append(links, Link{url, name, color, position, category})
 	}
 
-	render(w, "db", map[string]string{"Name": name, "Url": url, "Color": color})
+	sort.Sort(ByPosition(links))
+	render(w, "links", links)
 }
 
 func main() {
 	setup()
-	goji.Get("/hello/:name", hello)
-	goji.Get("/row", row)
+	goji.Get("/", links)
 	goji.Serve()
 }

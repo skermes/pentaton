2065e33916eb940dcb36d556c6aa095967a6755e
Sean Kermes <skermes@gmail.com>
2015-09-27 12:23:45 -0400

    Style new link input form.

--- templates/pentaton.js
+++ templates/pentaton.js
@@ -3,12 +3,29 @@
   var run = function() {
     var addLinkElem = document.getElementById('add-link');
     var trigger = addLinkElem.getElementsByClassName('activate')[0];
+    var colorInput = document.getElementById('add-link-color');
+    var urlInput = document.getElementById('add-link-url');
+    var nameInput = document.getElementById('add-link-name');
+
+    var changeLinkFormBackground = function() {
+      var color = colorInput.value;
+      addLinkElem.style.backgroundColor = '#' + color;
+    };
 
     var showNewLinkForm = function() {
       addLinkElem.className = 'active';
-    }
+      changeLinkFormBackground();
+    };
+
+    var selectAll = function(e) {
+      e.target.select();
+    };
 
     trigger.addEventListener('click', showNewLinkForm);
+    colorInput.addEventListener('input', changeLinkFormBackground)
+    colorInput.addEventListener('click', selectAll);
+    urlInput.addEventListener('click', selectAll);
+    nameInput.addEventListener('click', selectAll);
   }
 
   document.addEventListener('DOMContentLoaded', run);

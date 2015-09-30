6a55aee974933a5de0f0dc01b0300f1d6ed36f0a
Sean Kermes <skermes@gmail.com>
2015-09-30 14:57:29 -0400

    Barebones menu.

--- templates/pentaton.js
+++ templates/pentaton.js
@@ -6,6 +6,8 @@
     var colorInput = document.getElementById('add-link-color');
     var urlInput = document.getElementById('add-link-url');
     var nameInput = document.getElementById('add-link-name');
+    var menu = document.getElementById('menu');
+    var menuTrigger = document.getElementById('menu-trigger');
 
     var changeLinkFormBackground = function() {
       var color = colorInput.value;
@@ -21,11 +23,27 @@
       e.target.select();
     };
 
+    var closeMenu = function() {
+      menu.className = '';
+      document.body.removeEventListener('click', closeMenu);
+    };
+
+    var showMenu = function() {
+      menu.className = 'active';
+      document.body.addEventListener('click', closeMenu);
+    };
+
+    var stop = function(e) {
+      e.stopPropagation();
+    }
+
     trigger.addEventListener('click', showNewLinkForm);
     colorInput.addEventListener('input', changeLinkFormBackground)
     colorInput.addEventListener('click', selectAll);
     urlInput.addEventListener('click', selectAll);
     nameInput.addEventListener('click', selectAll);
+    menuTrigger.addEventListener('click', showMenu);
+    menu.addEventListener('click', stop);
   }
 
   document.addEventListener('DOMContentLoaded', run);
